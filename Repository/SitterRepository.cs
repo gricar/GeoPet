a1829b5117fec6fdc822411a7a3d1123e7c643d2
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-11-29 13:59:56 -0300

    feat: sitter repository

---
+++ Repository/SitterRepository.cs
@@ -0,0 +1,38 @@
+using GeoPet.Database.Context;
+using GeoPet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoPet.Repository;
+
+public class SitterRepository : ISitterRepository
+{
+  private readonly AppDbContext _context;
+
+  public SitterRepository(AppDbContext context)
+  {
+    _context = context;
+  }
+
+  public async Task<Sitter?> GetById(int id)
+  {
+    return await _context.Sitters.FindAsync(id);
+  }
+
+  public async Task Add(Sitter sitter)
+  {
+    _context.Sitters.Add(sitter);
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task Update(Sitter sitter)
+  {
+    _context.Entry(sitter).State = EntityState.Modified;
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task Delete(Sitter sitter)
+  {
+    _context.Sitters.Remove(sitter);
+    await _context.SaveChangesAsync();
+  }
+}
