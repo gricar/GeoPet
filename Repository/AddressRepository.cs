802aedfbf5eef06a9e6be8bdd8e7aa3967b7b275
Graciela Rocha <gracielasr694@gmail.com>
2022-12-10 17:17:46 -0300

    feat: AddressRepository

--- Repository/AddressRepository.cs
+++ Repository/AddressRepository.cs
@@ -1,6 +1,7 @@
 using GeoPet.Database.Context;
 using GeoPet.Repository.Interfaces;
 using GeoPet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeoPet.Repository;
 
@@ -10,5 +11,35 @@ public class AddressRepository : IAddressRepository
   public AddressRepository(AppDbContext context)
   {
     _context = context;
+  }
+
+  public async Task<IEnumerable<Address>> GetAll()
+  {
+    return await _context.Addresses.ToListAsync();
+  }
+
+  public async Task<Address?> GetById(int id)
+  {
+    return await _context.Addresses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(s => s.Id == id);
+  }
+
+  public async Task Add(Address address)
+  {
+    _context.Addresses.Add(address);
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task Update(Address address)
+  {
+    _context.Addresses.Update(address);
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task Delete(Address address)
+  {
+      _context.Addresses.Remove(address);
+      await _context.SaveChangesAsync();
   }
 }
--- Repository/Interfaces/IAddressRepository.cs
+++ Repository/Interfaces/IAddressRepository.cs
@@ -4,9 +4,9 @@ namespace GeoPet.Repository.Interfaces;
 
 public interface IAddressRepository
 {
-  public Task<IEnumerable<Address>> GetAll();
-  public Task<Address> GetById(int id);
-  public Task Add(Address address);
-  public Task Update(Address address);
-  public Task Delete(Address address);
+  Task<IEnumerable<Address>> GetAll();
+  Task<Address?> GetById(int id);
+  Task Add(Address address);
+  Task Update(Address address);
+ Task Delete(Address address);
 } 
