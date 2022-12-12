8cda2b7ebdd263ad4684349f8495f016e77ad93f
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-12 20:31:14 -0300

    refactor: create pet DTO

--- DTOs/PetDTO.cs
+++ DTOs/PetDTO.cs
@@ -10,5 +10,15 @@ public class PetDTO
   public PetSizesEnum Size { get; set; } = default!;
   public PetBreedsEnum Breed { get; set; } = default!;
   public bool IsLost { get; set; } = false;
+  public string SitterName { get; set; } = default!;
+}
+
+public class CreatePetDTO
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = default!;
+  public int Age { get; set; } = default!;
+  public PetSizesEnum Size { get; set; } = default!;
+  public PetBreedsEnum Breed { get; set; } = default!;
   public int SitterId { get; set; }
 }
