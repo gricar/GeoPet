1e9164c0c9729354edeec60159e804c2fa34a95a
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-13 19:17:35 -0300

    feat: repository - get sitter by credentials

--- Repository/Interfaces/ISitterRepository.cs
+++ Repository/Interfaces/ISitterRepository.cs
@@ -4,6 +4,7 @@ namespace GeoPet.Repository.Interfaces;
 
 public interface ISitterRepository
 {
+  Task<Sitter?> GetByEmailAndPwd(Sitter sitter);
   Task<Sitter?> GetById(int id);
   Task Add(Sitter sitter);
   Task Update(Sitter sitter);
